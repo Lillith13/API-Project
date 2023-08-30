47f2f9f42c457e856679c088049cee508f149db4
Lillith13 <krystina.bell@outlook.com>
2023-08-30 15:41:46 -0400

    finished steps 0 -> 2

--- backend/app.js
+++ backend/app.js
@@ -41,4 +41,41 @@ app.use(
 const routes = require("./routes");
 app.use(routes); //collect all routes
 
+// catch unhandled requests and forward to error handler
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+const { ValidationError } = require("sequelize");
+
+// process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a sequelize error
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = "Validation error";
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.log(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
 module.exports = app;
