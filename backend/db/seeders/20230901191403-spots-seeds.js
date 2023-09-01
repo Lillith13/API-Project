f374def346950fc5bb9f3312614585f991cb8b35
Lillith13 <krystina.bell@outlook.com>
2023-09-01 18:10:10 -0400

    added opts to spot seed

--- backend/db/seeders/20230901191403-spots-seeds.js
+++ backend/db/seeders/20230901191403-spots-seeds.js
@@ -26,27 +26,12 @@ const spotSeed = [
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
     await Spot.bulkCreate(spotSeed, {
       validate: true,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     options.tableName = "Spots";
     return queryInterface.bulkDelete(
       options,
