b4779226e2ad33a76328a16bc877f8451f4d0f64
Lillith13 <krystina.bell@outlook.com>
2023-09-06 17:18:07 -0400

    GAH!

--- backend/utils/reviewErrorCheckers.js
+++ backend/utils/reviewErrorCheckers.js
@@ -12,7 +12,7 @@ const reviewByUserExists = async (spotId, userId) => {
       userId,
     },
   });
-  console.log(review);
+  console.log(review.length);
   // review.dataValues ? true : false;
 };
 
