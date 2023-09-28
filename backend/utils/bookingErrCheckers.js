f443d076c30bffc904004e13388e556afba155d7
Lillith13 <krystina.bell@outlook.com>
2023-09-28 14:56:40 -0400

    fixing bookingConflicts middleware

--- backend/utils/bookingErrCheckers.js
+++ backend/utils/bookingErrCheckers.js
@@ -2,57 +2,37 @@ const { Booking } = require("../db/models");
 const { Op } = require("sequelize");
 async function bookingConflicts(req, _res, next) {
   const { startDate, endDate } = req.body;
+
   let err = new Error("Booking Conflict");
   err.errors = {};
   let errTriggered = false;
-  const booking = await Booking.findByPk(req.params.spotId, {
+
+  const bookingSD = await Booking.findByPk(req.params.spotId, {
     where: {
-      [Op.between]: [startDate, endDate],
+      startDate: {
+        [Op.between]: [startDate, endDate],
+      },
     },
-    attributes: ["startDate", "endDate"],
+  });
+  const bookingED = await Booking.findByPk(req.params.spotId, {
+    where: {
+      endDate: {
+        [Op.between]: [startDate, endDate],
+      },
+    },
   });
 
-  const bookedStart = booking["startDate"];
-  const bookedStartYear = bookedStart.getFullYear();
-  const bookedStartMonth = bookedStart.getMonth();
-  const bookedStartDay = bookedStart.getDate();
-
-  const bookedEnd = booking["endDate"];
-  const bookedEndYear = bookedEnd.getFullYear();
-  const bookedEndMonth = bookedEnd.getMonth();
-  const bookedEndDay = bookedEnd.getDate();
-
-  if (booking) {
-    if (
-      bookedStartYear === Number(startDate.split("/")[2]) ||
-      bookedEndYear === Number(endDate.split("/")[2])
-    ) {
-      // if desired startDate falls within an already booked timeframe
-      if (
-        Number(startDate.split("/")[0]) >= bookedStartMonth &&
-        Number(startDate.split("/")[1]) >= bookedStartDay &&
-        Number(startDate.split("/")[0]) <= bookedEndMonth &&
-        Number(startDate.split("/")[1]) <= bookedEndDay
-      ) {
-        err.errors.startDate = "Start date conflicts with an existing booking";
-        errTriggered = true;
-      }
-      // if desired endDate falls within an already booked timeFrame
-      if (
-        Number(endDate.split("/")[0]) >= bookedStartMonth &&
-        Number(endDate.split("/")[1]) >= bookedStartDay &&
-        Number(endDate.split("/")[0]) <= bookedEndMonth &&
-        Number(endDate.split("/")[1]) <= bookedEndDay
-      ) {
-        err.errors.endDate = "End date conflicts with an existing booking";
-        errTriggered = true;
-      }
-    }
+  if (bookingSD) {
+    err.errors.startDate = "Start date conflicts with an existing booking";
+    errTriggered = true;
+  }
+  if (bookingED) {
+    err.errors.endDate = "End date conflicts with an existing booking";
+    errTriggered = true;
   }
 
   if (errTriggered) {
-    err.message =
-      "Sorry, this spot is already booked for the specified dates, please check the spot's listed bookings and choose a timeframe outside of what has already been booked.";
+    err.message = "Sorry, this spot is already booked for the specified dates";
     err.status = 403;
     next(err);
   }
