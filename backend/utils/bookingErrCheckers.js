11bd6eb6f729399871cceec9d12202aa259abf1e
Lillith13 <krystina.bell@outlook.com>
2023-09-28 14:31:33 -0400

    minor tweaks on bookingConflicts middleware

--- backend/utils/bookingErrCheckers.js
+++ backend/utils/bookingErrCheckers.js
@@ -1,5 +1,5 @@
 const { Booking } = require("../db/models");
-const { Op } = require('sequelize')
+const { Op } = require("sequelize");
 async function bookingConflicts(req, _res, next) {
   const { startDate, endDate } = req.body;
   let err = new Error("Booking Conflict");
@@ -7,30 +7,43 @@ async function bookingConflicts(req, _res, next) {
   let errTriggered = false;
   const booking = await Booking.findByPk(req.params.spotId, {
     where: {
-      [Op.between]: [startDate, endDate]
+      [Op.between]: [startDate, endDate],
     },
     attributes: ["startDate", "endDate"],
   });
 
-  const bookedStart = booking["startDate"]
-  const startYear = bookedStart.getFullYear()
-  const startMonth = bookedStart.getMonth()
-  const startDay = bookedStart.getDate()
+  const bookedStart = booking["startDate"];
+  const startYear = bookedStart.getFullYear();
+  const startMonth = bookedStart.getMonth();
+  const startDay = bookedStart.getDate();
 
-  const bookedEnd = booking["endDate"]
-  const endYear = bookedEnd.getFullYear()
-  const endMonth = bookedEnd.getMonth()
-  const endDay = bookedEnd.getDate()
+  const bookedEnd = booking["endDate"];
+  const endYear = bookedEnd.getFullYear();
+  const endMonth = bookedEnd.getMonth();
+  const endDay = bookedEnd.getDate();
 
-  if(booking) {
-    if(startYear === startDate.getFullYear() || endYear === endDate.getFullYear()) {
+  if (booking) {
+    if (
+      startYear === startDate.getFullYear() ||
+      endYear === endDate.getFullYear()
+    ) {
       // if desired startDate falls within an already booked timeframe
-      if((startDate.getMonth() >= startMonth && startDate.getDate() >= startDay) && (startDate.getMonth() <= endMonth && startDate.getDate() <= endDay)) {
+      if (
+        startDate.getMonth() >= startMonth &&
+        startDate.getDate() >= startDay &&
+        startDate.getMonth() <= endMonth &&
+        startDate.getDate() <= endDay
+      ) {
         err.errors.startDate = "Start date conflicts with an existing booking";
         errTriggered = true;
       }
       // if desired endDate falls within an already booked timeFrame
-      if((endDate.getMonth() >= startMonth && endDate.getDate() >= startDay) && (endDate.getMonth() <= endMonth && endDate.getDate() <= endDay)) {
+      if (
+        endDate.getMonth() >= startMonth &&
+        endDate.getDate() >= startDay &&
+        endDate.getMonth() <= endMonth &&
+        endDate.getDate() <= endDay
+      ) {
         err.errors.endDate = "End date conflicts with an existing booking";
         errTriggered = true;
       }
@@ -38,7 +51,8 @@ async function bookingConflicts(req, _res, next) {
   }
 
   if (errTriggered) {
-    err.message = "Sorry, this spot is already booked for the specified dates, please check the spot's listed bookings and choose a timeframe outside of what has already been booked.";
+    err.message =
+      "Sorry, this spot is already booked for the specified dates, please check the spot's listed bookings and choose a timeframe outside of what has already been booked.";
     err.status = 403;
     next(err);
   }
