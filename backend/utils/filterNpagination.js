5a87791ab3001803d25b3eb68823d3cb6b14a55c
Lillith13 <krystina.bell@outlook.com>
2023-10-03 21:35:21 -0400

    testing pagination changes

--- backend/utils/filterNpagination.js
+++ backend/utils/filterNpagination.js
@@ -23,6 +23,11 @@ async function filterNpagi(req, _res, next) {
   else if (minPrice && !maxPrice) req.where.price = { [Op.gte]: minPrice };
   else if (maxPrice && !minPrice) req.where.price = { [Op.lte]: maxPrice };
 
+  const spotCount = await Spot.count({
+    where: req.where,
+  });
+  if (size >= spotCount) size = spotCount;
+
   req.pagination = {
     limit: size,
     offset: (page - 1) * size,
