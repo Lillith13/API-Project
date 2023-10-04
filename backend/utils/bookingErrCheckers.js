f613bd62de456a95fffbf3c8f903f302d9d5ed88
Lillith13 <krystina.bell@outlook.com>
2023-10-03 23:35:00 -0400

    fixing bookingErrCheck

--- backend/utils/bookingErrCheckers.js
+++ backend/utils/bookingErrCheckers.js
@@ -4,13 +4,12 @@ const { Op } = require("sequelize");
 async function bookingConflicts(req, _res, next) {
   const { startDate, endDate } = req.body;
 
-  let spotId
-  if(req.params.spotId) spotId = req.params.spotId
+  let spotId;
+  if (req.params.spotId) spotId = req.params.spotId;
   else {
-    const booking = await Booking.findByPk(req.params.bookingId)
-    spotId = booking.spotId
+    const booking = await Booking.findByPk(req.params.bookingId);
+    spotId = booking.spotId;
   }
-
 
   let err = new Error("Booking Conflict");
   err.errors = {};
@@ -62,15 +61,22 @@ async function bookingConflicts(req, _res, next) {
 async function bodyValidation(req, _res, next) {
   const { startDate, endDate } = req.body;
 
-  const sYear = startDate.split('/')[2]
-  const sMonth = startDate.split('/')[0]
-  const sDay = startDate.split('/')[1]
+  const start = {
+    Year: startDate.split("/")[2],
+    Month: startDate.split("/")[0],
+    Day: startDate.split("/")[1],
+  };
+  const end = {
+    Year: endDate.split("/")[2],
+    Month: endDate.split("/")[0],
+    Day: endDate.split("/")[1],
+  };
 
-  const eYear = endDate.split('/')[2]
-  const eMonth = endDate.split('/')[0]
-  const eDay = endDate.split('/')[1]
-
-  if (sYear > eYear || (sYear <= eYear && sMonth > eMonth) || (sYear <= eYear && sMonth <= eMonth && sDay > eDay)) {
+  if (
+    start.Year > end.Year ||
+    (start.Year <= end.Year && start.Month > end.Month) ||
+    (start.Year <= end.Year && start.Month <= end.Month && start.Day > end.Day)
+  ) {
     let err = new Error("Booking Conflict");
     err.errors = {};
     err.status = 400;
