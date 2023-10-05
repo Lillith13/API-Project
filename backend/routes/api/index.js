25af3eab0965bfa44dab07dcf5a0851aba50901f
Lillith13 <krystina.bell@outlook.com>
2023-10-05 16:02:19 -0400

    made documentation endpoint move permanent

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -55,61 +55,6 @@ router.delete(
   }
 );
 
-// display available endpoints on start-up -> /api
-/* router.get("/", async (req, res) => {
-  const availableEndPointsList = {
-    GET: {
-      "/api": "displays all available endpoints W/ brief description",
-      "/api/csrf/restore": "restores csrf token",
-      "/api/session":
-        "get currently signed in user -> displays null if no user signed in",
-      "/api/spots": "get all spots",
-      "/api/spots/:spotId": "get spots by spotId",
-      "/api/spots/current":
-        "returns all spots owned by the currently logged in user",
-      "/api/reviews/current":
-        "get all reviews written by currently signed in user",
-      "/api/spots/:spotId/reviews": "get all reviews by spotId",
-      "/api/bookings/current":
-        "get all bookings belonging to currently signed in user",
-      "/api/spots/:spotId/bookings": "get all bookings by spotId",
-    },
-    POST: {
-      "/api/session": "user log-in route",
-      "/api/users": "user sign-up route",
-      "/api/spots": "creates new spot owned by the currently signed in user",
-      "/api/spots/:spotId/images":
-        "add image to spot owned by currently signed in user",
-      "/api/spots/:spotId/reviews":
-        "add a new review for specified spot(spotId)",
-      "/api/reviews/:reviewId/images":
-        "add new image to currently signed in user's posted reviews",
-      "/api/spots/:spotId/bookings": "post new booking for specified spot",
-    },
-    PUT: {
-      "/api/spots/:spotId":
-        "edit spot owned by currently signed in user specified by spotId",
-      "/api/reviews/:reviewId":
-        "edit review written by currently signed in user specified by reviewId",
-      "/api/bookings/:bookingId":
-        "edit booking written by currently signed in user specified by bookingId",
-    },
-    DELETE: {
-      "/api/session": "user log-out route",
-      "/api/spots/:spotId":
-        "delete spot owned by currently signed in user specified by spotId",
-      "/api/reviews/:reviewId":
-        "delete review posted by currently signed in user specified by reviewId",
-      "/api/bookings/:bookingId":
-        "delete booking created by currently signed in user specified by bookingId",
-      "/api/spot-images/:imageId": "delete spot image by imgId",
-      "/api/review-images/:imageId": "delete review image by imgId",
-    },
-  };
-  // * add list of available tables (if can, when able)
-  res.json(availableEndPointsList);
-}); */
-
 router.post("/test", function (req, res) {
   res.json({
     requestBody: req.body,
