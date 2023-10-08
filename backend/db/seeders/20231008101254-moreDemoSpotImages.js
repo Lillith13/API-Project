77ab4cc2649431cc650b0fddccec9689f429dde3
Lillith13 <krystina.bell@outlook.com>
2023-10-08 13:21:03 -0400

    tidying branches

--- backend/db/seeders/20231008101254-moreDemoSpotImages.js
+++ backend/db/seeders/20231008101254-moreDemoSpotImages.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 
-const { Spot, SpotImage } = require("./backend/db/models");
+const { Spot, SpotImage } = require("../models");
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
